6cb1da319eca8e7cbbc6a70fb07c936470cc927d
Ezequiel Cuñado <ecunado@gmail.com>
2017-06-14 19:40:37 +0200

    Event handling added. Include some html inputs to ease plugin testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 declare var cordova;
 
@@ -10,33 +11,129 @@ declare var cordova;
 })
 export class HomePage {
 
-  public message: String;
+  private SHARED_SECRET: String = '0102030405060708091011121314151617181920212223242526272829303132';
 
-  constructor(public navCtrl: NavController, platform: Platform) {
+  public saleParams: any = {
+    amount: 1000,
+    currency: cordova ? cordova.plugins.Handpoint.Currency.GBP : 826
+  };
+  public macAddress: string = '68:AA:D2:02:89:B6';
+  public statusMessage: String;
+  public events: any[] = [];
+
+  constructor(
+    private _ngZone: NgZone,
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public platform: Platform) {
+    // Is cordova available ?
+    if (this.platform.is('cordova') && cordova !== undefined && cordova !== null) {
+      this.initSDK();
+    }
+  }
+
+  initSDK() {
     var that = this;
 
-    that.message = 'Loading…';
-    if (platform.is('cordova') && cordova !== undefined && cordova !== null) {
-      that.message = 'Loading… (Cordova available)';
-      try {
-        cordova.exec(
-          function (result) {
-            // TODO success
-            that.message = result;
-          },
-          function (error) {
-            // TODO error
-            that.message = 'Error running plugin ' + error;
-          },
-          'HandpointApiCordova',
-          'sale',
-          ['1']
-        );
-      } catch (ex) {
-        that.message = 'Exception';
-      }
+    // Set event handler function
+    that.configureEventHandler();
+    // Init SDK with shared secret
+    cordova.plugins.Handpoint.init({
+      sharedSecret: that.SHARED_SECRET,
+    }, function (result) {
+      // Connect to default device
+      cordova.plugins.Handpoint.connect({
+        device: {
+          name: "SureSwipe3708",
+          address: that.macAddress,
+          port: "1",
+          connectionMethod: cordova.plugins.Handpoint.ConnectionMethod.BLUETOOTH
+        }
+      }, function (result) {
+        that.statusMessage = 'Connected to device 68:AA:D2:02:89:B6';
+      }, function (error) {
+        that.statusMessage = 'Error connecting device ' + error;
+      });
+    }, function (error) {
+      that.statusMessage = 'Error on SDK init ' + error;
+    });
+  }
+
+  configureEventHandler() {
+    var that = this;
+
+    that.statusMessage = 'Registering events…';
+    if (that.platform.is('cordova') && cordova !== undefined && cordova !== null) {
+      that.statusMessage = 'Registering events… (Cordova available)';
+      // Configure event handler
+      cordova.plugins.Handpoint.eventHandler(function (event) {
+        // Run asynchronous call inside Angular execution context so data binding works
+        that._ngZone.run(() => {
+          that.events.push(event);
+        });
+      }, function (error) {
+        that.statusMessage = 'Error registering event handler ' + error;
+      });
     } else {
-      that.echoMessage = 'Plugin is not available on Browser platform';
+      that.statusMessage = 'Plugin is not available on Browser platform';
+    }
+  }
+
+  deviceDiscovery() {
+    var that = this;
+
+    that.statusMessage = 'Discovering devices…';
+    if (that.platform.is('cordova') && cordova !== undefined && cordova !== null) {
+      that.statusMessage = 'Discovering devices… (Cordova available)';
+      cordova.plugins.Handpoint.listDevices({
+        method: cordova.plugins.Handpoint.ConnectionMethod.SIMULATOR
+      }, function (result) {
+        that.events.push(result);
+      }, function (error) {
+        that.statusMessage = 'Error listDevices ' + error;
+      });
+    } else {
+      that.statusMessage = 'Plugin is not available on Browser platform';
+    }
+  }
+
+  /**
+   * Initiates a sale transaction
+   */
+  sale() {
+    var that = this;
+    that.statusMessage = 'Loading sale… ';
+    if (that.platform.is('cordova') && cordova !== undefined && cordova !== null) {
+      that.statusMessage = 'Loading sale… (Cordova available) ';
+      cordova.plugins.Handpoint.sale(that.saleParams, function (result) {
+        that.statusMessage = 'Sale completed ';
+      }, function (error) {
+        that.statusMessage = 'Error running plugin ' + error;
+      });
+    } else {
+      that.statusMessage = 'Plugin is not available on Browser platform';
+    }
+  }
+
+  connect(simulator: boolean) {
+    var that = this;
+
+    if (that.platform.is('cordova') && cordova !== undefined && cordova !== null) {
+      that.statusMessage = 'Connecting to ' + that.macAddress;
+      cordova.plugins.Handpoint.connect({
+        device: {
+          name: "SureSwipe3708",
+          address: that.macAddress,
+          port: "1",
+          connectionMethod: simulator ? cordova.plugins.Handpoint.ConnectionMethod.SIMULATOR : cordova.plugins.Handpoint.ConnectionMethod.BLUETOOTH
+        }
+      }, function (result) {
+        that.statusMessage = 'Connected to ' + that.macAddress;
+      }, function (error) {
+        that.statusMessage = 'Error connecting to ' + that.macAddress + ' ' + error;
+      });
+    } else {
+      that.statusMessage = 'Plugin is not available on Browser platform';
     }
   }
 
